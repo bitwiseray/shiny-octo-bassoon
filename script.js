a6608e0434a10158f8e948b5436d849d12b9be48
itzryan.777@gmail.com <itzryan.777@gmail.com>
2024-01-26 19:29:50 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 new Typed('#tiles', {
   strings: ['Developer', '3D Artist', 'Writer', 'Cook', 'Editor'],
   typeSpeed: 70,
-  backSpeed: 70,
+  backSpeed: 50,
   backDelay: 700,
   loop: true,
   showCursor: true
